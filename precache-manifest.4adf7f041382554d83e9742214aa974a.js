82facce7bcaade6bc03d5c0777a4908d33781c37
liuquan <lauchuen94@gmail.com>
2020-01-19 20:44:24 +0800

    Updates

---
+++ precache-manifest.4adf7f041382554d83e9742214aa974a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f7f767c52cc5be0f4495618ad41f095",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "7cba65daa95deadfb435",
+    "url": "/resume/static/css/main.89a7db49.chunk.css"
+  },
+  {
+    "revision": "815b60ad9b576a6df034",
+    "url": "/resume/static/js/2.df2da0bd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/resume/static/js/2.df2da0bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7cba65daa95deadfb435",
+    "url": "/resume/static/js/main.15703501.chunk.js"
+  },
+  {
+    "revision": "1d5c0405c2c30b0b6dac",
+    "url": "/resume/static/js/runtime-main.0a46d7cf.js"
+  }
+]);
